a7ac9dd976c4b21172e2bdbc695f8a7363f7f0f6
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-04-06 09:14:51 +0300

    done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,7 @@ class SessionsController < ApplicationController
         else
           render json: { error: 'Invalid username' }, status: :unprocessable_entity
         end
-      end
-    # def create
-    #     user = User.find_by(username: params[:username])
-    #     if user&.authenticate(params[:password])
-    #       session[:user_id] = user.id
-    #       render json: user, status: :created
-    #     else
-    #       render json: { error: "Invalid username or password" }, status: :unauthorized
-    #     end
-    #   end
-    
+      end    
       def destroy
         session.delete(:user_id)
         head :no_content
